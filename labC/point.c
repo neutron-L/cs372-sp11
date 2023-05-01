5c7ede1e6ef61bc7c88bd90208ad0b52f3860f90
rda <2055017970@qq.com>
2023-05-01 15:01:07 +0800

    start proj1

---
+++ labC/point.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <stdio.h>
+#include "point.h"
+
+/*
+ * Update *p by increasing p->x by x and 
+ * p->y by y
+ */
+void point_translate(Point *p, double x, double y)
+{
+  printf("Your job is to implement translate\n");
+  assert(0);
+}
+
+/*
+ * Return the distance from p1 to p2
+ */
+double point_distance(const Point *p1, const Point *p2)
+{
+  printf("Your job is to implement distance\n");
+  assert(0);
+  return -1.0;
+}
