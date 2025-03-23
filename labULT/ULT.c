d77dc43baff50eae094bb13464762e3c112f2795
rda <2055017970@qq.com>
2025-03-23 19:08:49 +0800

    ULT: 重构Init函数

--- labULT/ULT.c
+++ labULT/ULT.c
@@ -7,6 +7,7 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,6 +28,8 @@ static void stub(void (*root)(void *), void *arg);
    基本是不断pop + push + 判度当前队首，操作时间比较长
    可以考虑为queue添加一个search和remove push front的接口
  */
+// 分离线程分配的功能，因为在init时需要初始化scheduler和main thread
+static Tid ULT_NewThread(void (*fn)(void *), void *parg);
 static bool ULT_Search(ult_queue_t queue, Tid tid);
 static void ULT_Init();
 static void ULT_DeleteThread(ThrdCtlBlk *);
@@ -35,71 +38,21 @@ static void schedule(void *);
 
 Tid ULT_CreateThread(void (*fn)(void *), void *parg) {
     // assert(0); /* TBD */
+    Tid tid = ULT_INVALID;
+
     interruptsOff();
 
     ULT_Init();
 
-    Tid ret = ULT_INVALID;
-
-    // +1是因为我们额外创建了一个scheduler线程
-    if (thread_num >= ULT_MAX_THREADS + 1) {
-        ret = ULT_NOMORE;
-        goto end;
-    }
-    ThrdCtlBlk *tcb = NULL;
-    stack_t stack;
-    if ((tcb = (ThrdCtlBlk *)calloc(1, sizeof(ThrdCtlBlk))) == NULL) {
-        ret = ULT_NOMEMORY;
-        goto end;
-    }
-
-    if (fn != NULL) {
-
-        if ((stack.ss_sp = malloc(ULT_MIN_STACK)) == NULL) {
-            free(tcb);
-            ret = ULT_NOMEMORY;
-            goto end;
-        }
-        stack.ss_size = ULT_MIN_STACK;
-        stack.ss_flags = 0;
-
-        // 初始化上下文
-        getcontext(&tcb->ctx);
-
-        tcb->ctx.uc_stack = stack;
-        tcb->ctx.uc_link = NULL;
-        // makecontext(&tcb->ctx, (void (*)(void))fn, 1, parg); may not use this
-        tcb->ctx.uc_mcontext.gregs[REG_RIP] = (greg_t)stub;
-        tcb->ctx.uc_mcontext.gregs[REG_RDI] = (greg_t)fn;
-        tcb->ctx.uc_mcontext.gregs[REG_RSI] = (greg_t)parg;
-        tcb->ctx.uc_mcontext.gregs[REG_RSP] = (greg_t)(stack.ss_sp + ULT_MIN_STACK - 8);
-
-        // 此时上下文中信号是阻塞的，必须清空
-        assert(sigismember(&tcb->ctx.uc_sigmask, SIGALRM));
-        sigemptyset(&tcb->ctx.uc_sigmask);
-
-        // 初始时，main thread为当前running thread，随后由scheduler加入队列
-        ult_enqueue(ready_queue, tcb);
-    } else {
-        // 默认传入func为null的是初始化时创建main thread
-        if (running_thread != NULL) {
-            ret = ULT_INVALID;
-            goto end;
-        }
-        running_thread = tcb;
-    }
-
-    tcb->tid = next_tid;
-    tcb->state = RUNNABLE;
-    ++next_tid;
-    ++thread_num;
-    ret = tcb->tid;
+    // 获取当前进程的信号掩码
+    assert(!alarmIsEnabled());
+    tid = ULT_NewThread(fn, parg);
 
 end:
     interruptsOn();
-    return ret;
-}
 
+    return tid;
+}
 
 Tid ULT_Yield(Tid wantTid) {
     interruptsOff();
@@ -148,7 +101,7 @@ Tid ULT_Yield(Tid wantTid) {
 end:
     interruptsOn();
     return ret;
-}    
+}
 
 Tid ULT_DestroyThread(Tid tid) {
     interruptsOff();
@@ -165,7 +118,7 @@ Tid ULT_DestroyThread(Tid tid) {
         // 如果是最后一个线程，则会被schedule调度回来
         ret = ULT_NONE;
         goto end;
-    } 
+    }
     if (tid == ULT_ANY) {
         // 只剩下当前running线程和scheduler
         if (thread_num == 1 + 1) {
@@ -207,6 +160,61 @@ stub(void (*root)(void *), void *arg) {
     exit(0);                 // all threads are done, so process should exit
 }
 
+static Tid ULT_NewThread(void (*fn)(void *), void *parg) {
+    // +1是因为我们额外创建了一个scheduler线程
+    if (thread_num >= ULT_MAX_THREADS + 1) {
+        return ULT_NOMORE;
+    }
+    ThrdCtlBlk *tcb = NULL;
+
+    if ((tcb = (ThrdCtlBlk *)calloc(1, sizeof(ThrdCtlBlk))) == NULL) {
+        return ULT_NOMEMORY;
+    }
+
+    if (fn != NULL) {
+        stack_t stack;
+
+        if ((stack.ss_sp = malloc(ULT_MIN_STACK)) == NULL) {
+            free(tcb);
+            return ULT_NOMEMORY;
+        }
+        stack.ss_size = ULT_MIN_STACK;
+        stack.ss_flags = 0;
+
+        // 初始化上下文
+        getcontext(&tcb->ctx);
+
+        tcb->ctx.uc_stack = stack;
+        tcb->ctx.uc_link = NULL;
+        // makecontext(&tcb->ctx, (void (*)(void))fn, 1, parg); may not use this
+        tcb->ctx.uc_mcontext.gregs[REG_RIP] = (greg_t)stub;
+        tcb->ctx.uc_mcontext.gregs[REG_RDI] = (greg_t)fn;
+        tcb->ctx.uc_mcontext.gregs[REG_RSI] = (greg_t)parg;
+        tcb->ctx.uc_mcontext.gregs[REG_RSP] = (greg_t)(stack.ss_sp + ULT_MIN_STACK - 8);
+
+        // 此时上下文中信号是阻塞的，一般线程需要清空
+        assert(sigismember(&tcb->ctx.uc_sigmask, SIGALRM));
+        sigemptyset(&tcb->ctx.uc_sigmask);
+
+        // 初始时，main thread为当前running thread，随后由scheduler加入队列
+        ult_enqueue(ready_queue, tcb);
+    } else {
+        if (running_thread != NULL) {
+            return ULT_INVALID;
+        }
+        running_thread = tcb;
+    }
+
+    assert(!alarmIsEnabled());
+
+    tcb->tid = next_tid;
+    tcb->state = RUNNABLE;
+    ++next_tid;
+    ++thread_num;
+
+    return tcb->tid;
+}
+
 static bool ULT_Search(ult_queue_t queue, Tid wantTid) {
     int size = ult_queue_size(queue);
     while (size-- > 0 && ult_queue_front(queue)->tid != wantTid) {
@@ -220,18 +228,19 @@ static bool ULT_Search(ult_queue_t queue, Tid wantTid) {
 static void ULT_Init() {
     static bool inited = false;
 
+    assert(!alarmIsEnabled());
+
     if (inited) {
         return;
     }
     inited = true;
 
-
     // registerHandler();
     // 将当前thread(main thread)初始化并赋值running thread
     // main thread 得到tid 0
     ready_queue = ult_new_queue();
-    assert(ULT_isOKRet(ULT_CreateThread(NULL, NULL)) && running_thread != NULL);
-    assert(ULT_isOKRet(ULT_CreateThread(schedule, NULL))); // 这里有点奇怪，内部还会调用一次init，只不过什么也不做
+    assert(ULT_isOKRet(ULT_NewThread(NULL, NULL)) && running_thread != NULL);
+    assert(ULT_isOKRet(ULT_NewThread(schedule, NULL))); // 这里有点奇怪，内部还会调用一次init，只不过什么也不做
 
     scheduler = ult_dequeue(ready_queue);
     assert(ult_queue_is_empty(ready_queue));
@@ -239,7 +248,9 @@ static void ULT_Init() {
     // scheduler需要屏蔽信号
     sigemptyset(&scheduler->ctx.uc_sigmask);
     sigaddset(&scheduler->ctx.uc_sigmask, SIGALRM);
-    
+
+    assert(!alarmIsEnabled());
+
     swtch(running_thread, scheduler);
 }
 
@@ -254,6 +265,7 @@ static void swtch(ThrdCtlBlk *tcb1, ThrdCtlBlk *tcb2) {
     if (getcontext(&tcb1->ctx) == -1) {
         perror("getcontext");
     }
+    assert(!alarmIsEnabled());
     // 切换到目标上下文 ucp
     if (swtch_flag) {
         swtch_flag = 0;
@@ -269,9 +281,9 @@ static void schedule(void *_) {
     ucontext_t ctx;
 
     while (true) {
-      // 检查信号
-      getcontext(&ctx);
-      assert(sigismember(&ctx.uc_sigmask, SIGALRM) && sigismember(&scheduler->ctx.uc_sigmask, SIGALRM));
+        // 检查信号
+        getcontext(&ctx);
+        assert(sigismember(&ctx.uc_sigmask, SIGALRM) && sigismember(&scheduler->ctx.uc_sigmask, SIGALRM));
 
         ult_enqueue(ready_queue, running_thread);
 
