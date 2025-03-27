42fa595f712117ab56d2855dac64ef919c2e2d7a
rda <2055017970@qq.com>
2025-03-27 10:07:04 +0800

    FS: 实现事务id类

--- lab-FS/TransID.java
+++ lab-FS/TransID.java
@@ -9,14 +9,21 @@
  * (C) 2007 Mike Dahlin
  *
  */
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TransID{
 
   //
   // Implement this class
   //
+  private static final AtomicInteger nextTransID = new AtomicInteger(0);
+  public int transID;
+
 
   public TransID()
   {
+    transID = nextTransID.getAndIncrement();
   }
 
   
