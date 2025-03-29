1f2c21f321c9bfb430c7453812bc05bae7699cb9
rda <2055017970@qq.com>
2025-03-30 00:45:29 +0800

    FS: callbacktracker实现指定的tag的wait

--- lab-FS/CallbackTracker.java
+++ lab-FS/CallbackTracker.java
@@ -10,27 +10,77 @@
  *
  */
 
+import java.util.HashMap;
 import java.util.Vector;
+import java.util.Iterator;
+import java.util.concurrent.locks.Condition;
 
-public class CallbackTracker{
-    public void requestDone(DiskResult result){
+public class CallbackTracker implements DiskCallback {
+    private HashMap<Integer, DiskResult> doneTags;
+    private SimpleLock lock;
+    private Condition doneCond;
+
+    public CallbackTracker() {
+        doneTags = new HashMap<>();
+        lock = new SimpleLock();
+        doneCond = lock.newCondition();
+    }
+
+    @Override
+    public void requestDone(DiskResult result) {
         // TBD
+        try {
+            lock.lock();
+            doneTags.put(result.getTag(), result);
+            doneCond.signalAll();
+        } finally {
+            lock.unlock();
+        }
     }
 
     //
     // Wait for one tag to be done
     //
-    public DiskResult waitForTag(int tag){
+    public DiskResult waitForTag(int tag) {
         // TBD
-        return null;
+        DiskResult result = null;
+
+        try {
+            lock.lock();
+            while (!doneTags.containsKey(tag)) {
+                doneCond.awaitUninterruptibly();
+            }
+            result = doneTags.remove(tag);
+        } finally {
+            lock.unlock();
+        }
+        return result;
     }
 
     //
     // Wait for a set of tags to be done
     //
-    public Vector waitForTags(Vector tags){
+    public Vector<DiskResult> waitForTags(Vector<Integer> tags) {
         // TBD
-        return null;
+        Vector<DiskResult> resultVec = new Vector<>();
+
+        try {
+            lock.lock();
+            while (tags.size() != 0) {
+                doneCond.awaitUninterruptibly();
+                Iterator<Integer> iterator = tags.iterator();
+                while (iterator.hasNext()) {
+                    Integer tag = iterator.next();
+                    if (doneTags.containsKey(tag)) {
+                        resultVec.add(doneTags.remove(tag));
+                        iterator.remove();
+                    }
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+        return resultVec;
     }
 
     //
@@ -39,14 +89,12 @@ public class CallbackTracker{
     // When these results arrive, drop them on the
     // floor.
     //
-    public void dontWaitForTag(int tag){
+    public void dontWaitForTag(int tag) {
         // TBD
     }
 
-    public void dontWaitForTags(Vector tags){
+    public void dontWaitForTags(Vector<Integer> tags) {
         // TBD
     }
-
-
 
 }
