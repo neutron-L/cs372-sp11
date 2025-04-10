91ba9914ab83f8628e616129ade5000cfc4bd251
rda <2055017970@qq.com>
2025-04-10 21:49:19 +0800

    FS: flagfs一些简单包装接口的实现（除了读写数据）

--- lab-FS/FlatFS.java
+++ lab-FS/FlatFS.java
@@ -10,6 +10,8 @@
 
 import java.io.IOException;
 import java.io.EOFException;
+
+/* 是PTree的包装，简单地将一个tree作为文件，文件inode == tnum */
 public class FlatFS{
 
   public static final int ASK_MAX_FILE = 2423;
@@ -17,35 +19,41 @@ public class FlatFS{
   public static final int ASK_FREE_FILES = 29545;
   public static final int ASK_FILE_METADATA_SIZE = 3502;
 
+  private PTree ptree;
+
   public FlatFS(boolean doFormat)
     throws IOException
   {
+    ptree = new PTree(doFormat);
   }
 
   public TransID beginTrans()
   {
-    return null;
+    return ptree.beginTrans();
   }
 
   public void commitTrans(TransID xid)
     throws IOException, IllegalArgumentException
   {
+    ptree.commitTrans(xid);
   }
 
   public void abortTrans(TransID xid)
     throws IOException, IllegalArgumentException
   {
+    ptree.abortTrans(xid);
   }
 
   public int createFile(TransID xid)
     throws IOException, IllegalArgumentException
   {
-    return -1;
+    return ptree.createTree(xid);
   }
 
   public void deleteFile(TransID xid, int inumber)
     throws IOException, IllegalArgumentException
   {
+    ptree.deleteTree(xid, inumber);
   }
 
   public int read(TransID xid, int inumber, int offset, int count, byte buffer[])
@@ -63,18 +71,30 @@ public class FlatFS{
   public void readFileMetadata(TransID xid, int inumber, byte buffer[])
     throws IOException, IllegalArgumentException
   {
+    ptree.readTreeMetadata(xid, inumber, buffer);
   }
 
 
   public void writeFileMetadata(TransID xid, int inumber, byte buffer[])
     throws IOException, IllegalArgumentException
   {
+    ptree.writeTreeMetadata(xid, inumber, buffer);
   }
 
   public int getParam(int param)
     throws IOException, IllegalArgumentException
   {
-    return -1;
+    if (param == ASK_MAX_FILE ) {
+      return ptree.getParam(PTree.ASK_MAX_TREES);
+    } else if (param == ASK_FREE_SPACE_BLOCKS ) {
+      return ptree.getParam(PTree.ASK_FREE_SPACE);
+    } else if (param == ASK_FREE_FILES ) {
+      return ptree.getParam(PTree.ASK_FREE_TREES);
+    } else if (param == ASK_FILE_METADATA_SIZE) {
+      return PTree.METADATA_SIZE;
+    } else {
+      throw new IllegalArgumentException("Bad Param");
+    }
   }
     
 
