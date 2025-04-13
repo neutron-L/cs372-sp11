4567ffb9b69b69d8c089858dd1b3ff5aedd1f60e
rda <2055017970@qq.com>
2025-04-13 10:57:24 +0800

    FS: Common添加字符串和字节数组转换接口

--- lab-FS/Common.java
+++ lab-FS/Common.java
@@ -3,6 +3,9 @@
  * This file represents the constants and basic data struct to your 
  * file system.
  */
+
+import java.io.UnsupportedEncodingException;
+
 public class Common {
 
   public static final int ADISK_REDO_LOG_SECTORS = 1024;
@@ -66,6 +69,44 @@ public class Common {
       b[ii] = value;
     }
     return;
+  }
+
+  // 使用utf-8字符集
+  public static String byteArr2String(byte[] bytes) {
+    if (bytes == null) {
+      return null;
+    }
+    String str = null;
+    
+    try {
+        // 指定 UTF-8 字符集将字节数组转换为字符串
+        str = new String(bytes, "UTF-8"); 
+        System.out.println(str);
+    } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+        return null;
+    } 
+
+    return str;
+  }
+
+  // 使用utf-8字符集
+  public static byte[] String2byteArr(String str) {
+    byte[] bytes = null;
+
+    if (str == null) {
+      return bytes;
+    }
+    
+    try {
+        // 指定 UTF-8 字符集将字节数组转换为字符串
+        bytes = str.getBytes("UTF-8"); 
+    } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+        return null;
+    } 
+
+    return bytes;
   }
 
 }
